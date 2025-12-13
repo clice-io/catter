424a7de083f62200ef7b4bdfbd5c4f0536fa7985
star9029 <hengxings783@gmail.com>
2025-12-14 00:52:38 +0800

    fix pnpm not found

--- src/common/libconfig/include/libconfig/js-lib.h
+++ src/common/libconfig/include/libconfig/js-lib.h
@@ -8,5 +8,5 @@ extern "C" {
 }
 
 namespace catter::config::data {
-    const std::string_view js_lib{_binary_lib_js_start, _binary_lib_js_end};
+const std::string_view js_lib{_binary_lib_js_start, _binary_lib_js_end};
 }  // namespace catter::config::data
--- src/common/libconfig/include/libconfig/js-test.h
+++ src/common/libconfig/include/libconfig/js-test.h
@@ -3,6 +3,6 @@
 #include <string_view>
 
 namespace catter::config::data {
-    /// Use in test, the path of js test files
-    constexpr std::string_view js_test_path = JS_TEST_PATH;
+/// Use in test, the path of js test files
+constexpr std::string_view js_test_path = JS_TEST_PATH;
 }  // namespace catter::config::data
