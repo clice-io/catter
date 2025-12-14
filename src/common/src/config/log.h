e934803b6b2209a17378aa96b9698520a2067656
seele <seele@gmail.com>
2025-12-14 21:54:35 +0800

    Add missing newline after pragma once in log.h for consistency

--- src/common/src/config/log.h
+++ src/common/src/config/log.h
@@ -1,11 +1,12 @@
+#pragma once
+
 namespace catter::config::log {
 /// like [2024-06-15 14:23:01.123] [INFO] [pid:12345][tid:123] message
 #ifdef DEBUG
 constexpr static char LOG_PATTERN_FILE[] =
     "[%@] [%Y-%m-%d %H:%M:%S.%e] [%l] [pid:%P][tid:%t] \n %v";
 #else
-constexpr static char LOG_PATTERN_FILE[] =
-    "[%Y-%m-%d %H:%M:%S.%e] [%l] [pid:%P][tid:%t] \n %v";
+constexpr static char LOG_PATTERN_FILE[] = "[%Y-%m-%d %H:%M:%S.%e] [%l] [pid:%P][tid:%t] \n %v";
 #endif
 constexpr static char LOG_PATTERN_CONSOLE[] = "%v";
 
