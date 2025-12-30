79216b5070e9d88390c74b68cf90fb142fd63497
fogsong233 <fogsong233@gmail.com>
2025-12-30 21:47:28 +0800

    Resovle comments

--- src/common/opt-data/catter-proxy/table.h
+++ src/common/opt-data/catter-proxy/table.h
@@ -3,7 +3,7 @@
 #include "option/opt_table.h"
 
 namespace catter::optdata::catter_proxy {
-enum OptionID { OPT_INVALID = 0, OPT_INPUT = 1, OPT_UNKNOWN = 2, OPT_PARENT_ID = 2, OPT_EXEC = 3 };
+enum OptionID { OPT_INVALID = 0, OPT_INPUT = 1, OPT_UNKNOWN = 2, OPT_PARENT_ID, OPT_EXEC };
 
 extern opt::OptTable catter_proxy_opt_table;
 }  // namespace catter::optdata::catter_proxy
