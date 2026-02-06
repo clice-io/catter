6bc56af850541c5f224feabdec2eb990b8b7e537
seele <seele@gmail.com>
2026-02-06 14:10:31 +0800

    refactor: simplify split2vec implementation for clarity

--- tests/unit/common/option/util.h
+++ tests/unit/common/option/util.h
@@ -5,7 +5,7 @@
 #include <string>
 
 inline auto split2vec(std::string_view str) {
-    return std::views::split(str, ' ') |
-           std::views::transform([](auto&& rng) { return std::string(rng.begin(), rng.end()); }) |
-           std::ranges::to<std::vector<std::string>>();
+    auto views = std::views::split(str, ' ') |
+           std::views::transform([](auto&& rng) { return std::string(rng.begin(), rng.end()); });
+    return std::vector<std::string>(views.begin(), views.end());
 };
