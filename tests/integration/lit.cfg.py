55515330a68ce96e2c2fd628d365ea4d9d2ccb85
fogsong233 <fogsong233@gmail.com>
2026-02-07 15:37:24 +0800

    add IT CI

--- tests/integration/lit.cfg.py
+++ tests/integration/lit.cfg.py
@@ -1,15 +1,30 @@
 # ruff: noqa: F821
 import sys
 import os
-import lit.formats
 import subprocess
+import json
+from typing import Callable
+import lit.formats
+
+
+def get_cmd_output(cmd: str, fn: Callable[[str], str]) -> str:
+    res = ""
+    try:
+        res = fn(subprocess.getoutput(cmd))
+    except Exception as e:
+        print(f"Error parsing output of  {cmd}: {e}")
+    if not res:
+        raise RuntimeError("Could not find info from {cmd}")
+    return res
 
 
 config.name = "Catter Integration Test"
 config.test_format = lit.formats.ShTest(True)
 config.suffixes = [".js", ".c", ".cpp", ".cc", ".ts"]
 
-project_root = os.path.abspath(os.path.join(__file__, "..", "..", ".."))
+project_root = get_cmd_output(
+    "xmake show --json", lambda r: json.loads(r)["project"]["projectdir"]
+)
 
 config.test_source_root = os.path.join(project_root, "tests", "integration", "test")
 config.test_exec_root = os.path.join(project_root, "build", "lit-tests")
@@ -18,27 +33,13 @@ is_macos = sys.platform == "darwin"
 is_linux = sys.platform.startswith("linux")
 
 if is_linux or is_macos:
-    hook_path = ""
-    try:
-        xmake_output = subprocess.getoutput("xmake show -t catter-hook-unix")
-
-        lib_suffix = ".dylib" if is_macos else ".so"
-
-        for line in xmake_output.splitlines():
-            line = line.strip()
-            if "targetfile" in line:
-                raw_path = (
-                    line.split(":", 1)[1].split(lib_suffix)[0].strip() + lib_suffix
-                )
-                hook_path = os.path.join(project_root, raw_path)
-                break
-    except Exception as e:
-        print(f"Error parsing xmake output: {e}")
-
-    if not hook_path:
-        raise RuntimeError(
-            "Could not find catter-hook-unix target file path from xmake"
-        )
+    hook_path = get_cmd_output(
+        "xmake show -t catter-hook-unix --json", lambda r: json.loads(r)["targetfile"]
+    )
+    hook_path = os.path.join(project_root, hook_path)
+    mode = get_cmd_output(
+        "xmake show --json", lambda r: json.loads(r)["project"]["mode"]
+    )
 
     preload_var_name = "DYLD_INSERT_LIBRARIES" if is_macos else "LD_PRELOAD"
 
@@ -50,11 +51,16 @@ if is_linux or is_macos:
 
     config.substitutions.append(("%catter-proxy", catter_proxy_cmd))
     config.substitutions.append(("%catter-hook-path", hook_path))
+    config.substitutions.append(("%filecheck", "FileCheck"))
 
+    # in debug mode, we need asan
     if is_macos:
         config.substitutions.append(("%cc", "clang++ -std=c++23 -fuse-ld=lld %s -o %t"))
     else:
         config.substitutions.append(("%cc", "g++ -std=c++23 %s -o %t"))
+        if mode == "debug":
+            asan_path = get_cmd_output("g++ -print-file-name=libasan.so", lambda x: x)
+            hook_path = f"{asan_path}:{hook_path}"
 
     inject_cmd_str = (
         f"{preload_var_name}={hook_path} "
