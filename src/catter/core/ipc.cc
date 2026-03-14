64b326b1892b8efe6c8f223e7f3c9de561ff523b
seele <seele@gmail.com>
2026-03-15 03:30:30 +0800

    refactor: streamline request handling in Dispatcher and improve error logging

--- src/catter/core/ipc.cc
+++ src/catter/core/ipc.cc
@@ -26,23 +26,6 @@ using namespace data;
 template <typename T>
 struct Helper;
 
-template <typename... Args>
-auto deserialize_args(BufferReader& buf_reader) {
-    return std::tuple<Args...>{Serde<Args>::deserialize(buf_reader)...};
-}
-
-template <typename Ret, typename... Args>
-struct Helper<Ret(Args...)> {
-    static auto read(BufferReader& buf_reader) {
-        return deserialize_args<Args...>(buf_reader);
-    }
-};
-
-template <Request Req>
-eventide::task<void> handle_req(BufferReader& buf_reader) {
-    return Helper<RequestType<Req>>::handle(buf_reader);
-}
-
 template <auto... MemFns>
 struct Dispatcher {
     using Class = std::common_type_t<typename eventide::mem_fn<MemFns>::ClassType...>;
@@ -63,9 +46,11 @@ struct Dispatcher {
                 return static_cast<size_t>(-1);
             }
         }(std::in_place_index<0>);
-        static_assert(idx != static_cast<size_t>(-1),
-                      "No matching request type found in dispatcher");
-        return std::get<idx>(Tuple);
+        if constexpr(idx == static_cast<size_t>(-1)) {
+            return nullptr;
+        } else {
+            return std::get<idx>(Tuple);
+        }
     }
 
     template <typename T>
@@ -75,7 +60,7 @@ struct Dispatcher {
     struct Helper<Ret(Args...)> {
         static std::optional<std::vector<char>> call(eventide::function_ref<Ret(Args...)> callback,
                                                      BufferReader& buf_reader) {
-            auto args_tuple = deserialize_args<Args...>(buf_reader);
+            auto args_tuple = std::tuple<Args...>{Serde<Args>::deserialize(buf_reader)...};
             if constexpr(std::is_void_v<Ret>) {
                 std::apply(callback, args_tuple);
                 return std::nullopt;
@@ -89,14 +74,22 @@ struct Dispatcher {
     static std::optional<std::vector<char>> dispatch(Class& obj, BufferReader& buf_reader) {
         using ReflRequest = eventide::refl::reflection<Request>;
         Request req = Serde<Request>::deserialize(buf_reader);
-
+        LOG_INFO("Handling request of type: {}", eventide::refl::enum_name(req));
         return [&]<size_t I = 0>(this const auto& self) -> std::optional<std::vector<char>> {
             if constexpr(I < ReflRequest::member_count) {
                 constexpr auto val = ReflRequest::member_values[I];
                 if(val == req) {
-                    return Helper<RequestType<val>>::call(
-                        eventide::bind_ref<match_mem_fn<val>()>(obj),
-                        buf_reader);
+                    constexpr auto mem_fn = match_mem_fn<val>();
+                    if constexpr(mem_fn == nullptr) {
+                        LOG_INFO("No matching member function found for request type: {}",
+                                 eventide::refl::enum_name(req));
+                        throw std::runtime_error(
+                            std::format("No matching member function found for request type: {}",
+                                        eventide::refl::enum_name(req)));
+                    } else {
+                        return Helper<RequestType<val>>::call(eventide::bind_ref<mem_fn>(obj),
+                                                              buf_reader);
+                    }
                 }
                 return self.template operator()<I + 1>();
             } else {
@@ -176,7 +169,6 @@ eventide::task<void> accept(std::unique_ptr<InjectService> service, eventide::pi
         }
 
         BufferReader buf_reader(*req_packet);
-        Request req = Serde<Request>::deserialize(buf_reader);
 
         using DispatcherType = Dispatcher<&InjectService::create,
                                           &InjectService::make_decision,
