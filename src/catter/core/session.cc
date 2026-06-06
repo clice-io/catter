f41f1c6f46752fda48e123bde464e075bc9aebd2
seele <seele@gmail.com>
2026-06-06 22:16:29 +0800

    refactor: rename output_mode to StdioMode and update related code

--- src/catter/core/session.cc
+++ src/catter/core/session.cc
@@ -40,7 +40,7 @@ kota::task<data::process_result> Session::run(RunPlan run_plan) {
     auto spawn_task = this->spawn(std::move(run_plan.launch_plan.executable),
                                   std::move(run_plan.launch_plan.args),
                                   std::move(run_plan.launch_plan.cwd),
-                                  run_plan.launch_plan.output_mode);
+                                  run_plan.launch_plan.mode);
 
     auto [_, process_result] = co_await kota::when_all{std::move(loop_task), std::move(spawn_task)};
     co_return std::move(process_result);
@@ -79,7 +79,7 @@ kota::task<void> Session::loop(ClientAcceptor acceptor) {
 kota::task<data::process_result> Session::spawn(std::string executable,
                                                 std::vector<std::string> args,
                                                 std::string cwd,
-                                                output_mode output_mode) {
+                                                StdioMode mode) {
     // for exception safety: ensure acceptor is stopped when spawn exits, since spawn failure should
     // prevent the session from running
     auto guard = util::make_guard([&]() noexcept {
@@ -112,10 +112,10 @@ kota::task<data::process_result> Session::spawn(std::string executable,
                      kota::process::stdio::pipe(false, true)}
     };
 
-    switch(output_mode) {
-        case output_mode::inherit:
+    switch(mode) {
+        case StdioMode::inherit:
             co_return co_await capture_process_result(make_process_event(opts), stdout, stderr);
-        case output_mode::capture:
+        case StdioMode::capture:
             co_return co_await capture_process_result(make_process_event(opts), nullptr, nullptr);
     }
 
