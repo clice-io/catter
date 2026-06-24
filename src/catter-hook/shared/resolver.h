45ef3949d961b7e1ee6043180d55d09575409b17
seele <seele@gmail.com>
2026-06-24 12:04:49 +0800

    docs: add comments explaining executable file resolution in resolver.h

--- src/catter-hook/shared/resolver.h
+++ src/catter-hook/shared/resolver.h
@@ -18,6 +18,16 @@ std::basic_string<CharT> resolve_command_line_token(std::basic_string_view<CharT
 
 #else
 
+// The execlp(), execvp(), and execvpe() functions duplicate the actions of the shell in searching
+// for an executable file if the specified filename does not contain a slash (/) character. The file
+// is sought in the colon-separated list of directory pathnames specified in the PATH environment
+// variable. If this variable isn't defined, the path list defaults to the current directory
+// followed by the list of directories returned by confstr(_CS_PATH). (This confstr call typically
+// returns the value "/bin:/usr/bin".)
+//
+// If the specified filename includes a slash character, then PATH is ignored, and the file at the
+// specified pathname is executed.
+
 [[nodiscard]]
 std::expected<std::filesystem::path, int> resolve_path_like(std::string_view file);
 
